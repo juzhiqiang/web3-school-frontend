6d2d2964acf5e2632bd588199fbaf8c07d4728a8
miss you <1020814597@qq.com>
2025-09-01 11:18:59 +0800

    Add Web3 configuration file

---
+++ src/config/web3.ts
@@ -0,0 +1,9 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains'
+
+export const config = getDefaultConfig({
+  appName: 'Web3 School',
+  projectId: process.env.VITE_WALLET_CONNECT_PROJECT_ID || 'your-project-id',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: false, // If your dApp uses server side rendering (SSR)
+})
