c596a6f67da66f75f20e9eb47f34aa40d3c3c247
juzhiqiang <1020814597@qq.com>
2025-09-01 11:27:33 +0800

    feat: update

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -1,6 +1,5 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains'
-
 export const config = getDefaultConfig({
   appName: 'Web3 School',
   projectId: process.env.VITE_WALLET_CONNECT_PROJECT_ID || 'your-project-id',
