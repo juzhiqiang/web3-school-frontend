fba87a0eebb6fa5c31f96920bff8d9602f9f653b
miss you <1020814597@qq.com>
2025-09-01 13:42:36 +0800

    Add NetworkStatus component to Layout for testnet warnings

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Header from './Header'
 import Footer from './Footer'
+import NetworkStatus from '../common/NetworkStatus'
 
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
+      <NetworkStatus />
       <main className="flex-1">
         <Outlet />
       </main>
