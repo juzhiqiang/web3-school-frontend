d1b1f3cccb4b6f9aadba40a04e514592356e7887
miss you <1020814597@qq.com>
2025-09-03 16:49:42 +0800

    Update Layout component to include DevTools for development environment

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Header from './Header'
 import Footer from './Footer'
 import NetworkStatus from '../common/NetworkStatus'
+import DevTools from '../common/DevTools'
 
 const Layout: React.FC = () => {
+  const [showDevTools, setShowDevTools] = useState(false)
+  const isDevelopment = import.meta.env.MODE === 'development'
+  
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
@@ -14,6 +18,15 @@ const Layout: React.FC = () => {
         <Outlet />
       </main>
       <Footer />
+      
+      {/* 开发工具 - 仅在开发环境显示 */}
+      {isDevelopment && (
+        <DevTools 
+          isVisible={showDevTools}
+          onToggle={() => setShowDevTools(!showDevTools)}
+        />
+      )}
+      
       <Toaster
         position="top-right"
         toastOptions={{
