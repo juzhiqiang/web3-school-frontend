69c51e74a64c828fd94972637dd8c43c4106e5ab
miss you <1020814597@qq.com>
2025-09-05 00:25:30 +0800

    Update App.tsx to use protected course routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import toast, { Toaster } from 'react-hot-toast'
 import { config } from './config/web3'
 import { Web3Provider } from './contexts/Web3Context'
 import Layout from './components/Layout/Layout'
+import ProtectedCourseRoute from './components/ProtectedCourseRoute/ProtectedCourseRoute'
 import CourseListing from './pages/CourseListing/CourseListing'
 import CreateCourse from './pages/CreateCourse/CreateCourse'
 import Financial from './pages/Financial/Financial'
@@ -31,10 +32,35 @@ function App() {
                   <Route path="financial" element={<Financial />} />
                   <Route path="token-swap" element={<TokenSwap />} />
                   <Route path="profile" element={<Profile />} />
-                  <Route path="course/:id" element={<CourseDetails />} />
+                  
+                  {/* 受保护的课程路由 - 需要购买后才能访问 */}
+                  <Route 
+                    path="course/:id" 
+                    element={
+                      <ProtectedCourseRoute>
+                        <CourseDetails />
+                      </ProtectedCourseRoute>
+                    } 
+                  />
+                  <Route 
+                    path="course/:id/learn" 
+                    element={
+                      <ProtectedCourseRoute>
+                        <CourseDetails learn />
+                      </ProtectedCourseRoute>
+                    } 
+                  />
+                  <Route 
+                    path="course/:id/details" 
+                    element={
+                      <ProtectedCourseRoute>
+                        <CourseDetails details />
+                      </ProtectedCourseRoute>
+                    } 
+                  />
+                  
+                  {/* 预览路由不受保护 - 但CourseDetails内部会处理预览逻辑 */}
                   <Route path="course/:id/preview" element={<CourseDetails preview />} />
-                  <Route path="course/:id/learn" element={<CourseDetails learn />} />
-                  <Route path="course/:id/details" element={<CourseDetails details />} />
                 </Route>
               </Routes>
               
