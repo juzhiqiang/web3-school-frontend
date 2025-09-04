2bc7d557ddf86b3418b978d04e7c16a4abe086d7
miss you <1020814597@qq.com>
2025-09-04 11:29:27 +0800

    Add index file for common components export

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+// Common components exports
+export { default as RichTextEditor } from './RichTextEditor';
+export { default as LessonManager } from './LessonManager';
+export { default as TagInput } from './TagInput';
+
+// Re-export types for convenience
+export type { CourseLesson, Course, CreateCourseFormData } from '../../types/course';
